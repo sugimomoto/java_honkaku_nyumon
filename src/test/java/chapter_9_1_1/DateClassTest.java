9699668d9ea820a969bacb5ec293726142dfd5f4
kazuya sugimoto <sugimomoto@gmail.com>
2021-10-21 07:35:22 +0900

    Added chapter 9_1_2, 9_1_3

--- src/test/java/chapter_9_1_1/DateClassTest.java
+++ src/test/java/chapter_9_1_1/DateClassTest.java
@@ -2,6 +2,7 @@ package chapter_9_1_1;
 
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -20,12 +21,29 @@ public class DateClassTest {
         Date after5000 = new Date(5000);
         System.out.println(after5000);
 
+        assertTrue("success", now.after(after5000));
     }
 
     @Test
     public void CalendarClassMethodTest(){
         Calendar calendar = Calendar.getInstance();
         System.out.println(calendar);
+
+        calendar.set(2013,9,22,18,36,42);
+
+        assertEquals(2013, calendar.get(Calendar.YEAR));
+        assertEquals(9, calendar.get(Calendar.MONTH));
+
+        calendar.add(Calendar.YEAR, 2);
+
+        assertEquals(2015, calendar.get(Calendar.YEAR));
+
+        calendar.add(Calendar.MONTH, -2);
+        assertEquals(7, calendar.get(Calendar.MONTH));
+
+        Date now = new Date();
+
+        assertTrue("message", now.after(calendar.getTime()));
 
     }
 }
