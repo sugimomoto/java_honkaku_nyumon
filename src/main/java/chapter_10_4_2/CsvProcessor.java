39c9789089ea916800afb2a9b878c6660ff92bf3
kazuya sugimoto <sugimomoto@gmail.com>
2021-10-29 07:40:05 +0900

    Added chapter_10_4_2

---
+++ src/main/java/chapter_10_4_2/CsvProcessor.java
@@ -0,0 +1,13 @@
+package chapter_10_4_2;
+
+import static chapter_10_4_2.UserCsvColumn.*;
+
+public class CsvProcessor {
+    public void processColumn(int column){
+
+        // 通常、「定数クラス名.定数名」で指定するが、staticインポートの場合は「定数名」だけでOK
+        if(column == NAME){
+
+        }
+    }
+}
---
+++ src/main/java/chapter_10_4_2/UserCsvColumn.java
@@ -0,0 +1,7 @@
+package chapter_10_4_2;
+
+public class UserCsvColumn {
+    public static final int NAME = 0;
+    public static final int ADDRESS = 1;
+    public static final int MAIL = 2;
+}
