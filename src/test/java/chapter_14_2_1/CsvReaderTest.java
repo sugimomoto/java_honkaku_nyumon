13fe8ef31eb009cad3f5df26080f8e6813b3759b
sugimomoto <sugimomoto@gmail.com>
2021-11-30 07:21:37 +0900

    Fixed CSV sample data and unit tests

--- src/test/java/chapter_14_2_1/CsvReaderTest.java
+++ src/test/java/chapter_14_2_1/CsvReaderTest.java
@@ -2,6 +2,7 @@ package chapter_14_2_1;
 
 import static org.junit.Assert.assertEquals;
 
+import java.sql.Date;
 import java.util.List;
 
 import org.junit.Test;
@@ -15,6 +16,9 @@ public class CsvReaderTest {
         List<Employee> employees = reader.readEmployee();
 
         assertEquals(2, employees.size());
-        assertEquals("山田 太郎", employees.get(1).getName());
+        assertEquals("山田 太郎", employees.get(0).getName());
+        assertEquals((Integer)28, employees.get(1).getAge());
+        assertEquals(Date.valueOf("1978-4-1"), employees.get(0).getBirth());
+        assertEquals(null, employees.get(1).getNote());
     }
 }
