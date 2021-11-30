67b5f5e22f21d8a20851ee8e17bcb80a77b7a2e7
sugimomoto <sugimomoto@gmail.com>
2021-12-01 07:40:56 +0900

    Added CSV Writer

--- src/main/java/chapter_14_2_1/EmployeeReader.java
+++ src/main/java/chapter_14_2_1/EmployeeReader.java
@@ -4,15 +4,46 @@ import java.io.IOException;
 import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.nio.file.Paths;
 
 import org.supercsv.cellprocessor.*;
 import org.supercsv.cellprocessor.constraint.*;
 import org.supercsv.cellprocessor.ift.CellProcessor;
 import org.supercsv.io.CsvBeanReader;
+import org.supercsv.io.CsvBeanWriter;
 import org.supercsv.io.ICsvBeanReader;
+import org.supercsv.io.ICsvBeanWriter;
 import org.supercsv.prefs.CsvPreference;
 
 public class EmployeeReader {
+
+    private Path path;
+
+    public EmployeeReader(String filePath){
+        path = Paths.get(filePath);
+    }
+
+    public void writeEmployee(List<Employee> employees) {
+        String[] header = new String[]{"name","age","birth","email","note"};
+
+        CellProcessor[] processors = new CellProcessor[]{
+            new NotNull(),
+            new NotNull(),
+            new FmtDate("yyyy/MM/dd"),
+            new NotNull(),
+            new Optional()
+        };
+
+        try(ICsvBeanWriter beanWriter = new CsvBeanWriter(Files.newBufferedWriter(path), CsvPreference.STANDARD_PREFERENCE)){
+            beanWriter.writeHeader(header);
+            for (Employee employee : employees) {
+                beanWriter.write(employee, header, processors);
+            }
+        
+        } catch (IOException e) {
+            // error handling
+        }
+    }
     
     public List<Employee> readEmployee(){
         List<Employee> employees = new ArrayList();
@@ -24,8 +55,6 @@ public class EmployeeReader {
             new StrRegEx("[a-z0-9\\._]+@[a-z0-9\\.]+"),
             new Optional()
         };
-
-        Path path = Paths.get("employee.csv");
 
         try(ICsvBeanReader beanReader = new CsvBeanReader(Files.newBufferedReader(path), CsvPreference.STANDARD_PREFERENCE)){
             String[] header = beanReader.getHeader(true);
--- src/test/java/chapter_14_2_1/CsvReaderTest.java
+++ src/test/java/chapter_14_2_1/CsvReaderTest.java
@@ -3,6 +3,7 @@ package chapter_14_2_1;
 import static org.junit.Assert.assertEquals;
 
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Test;
@@ -11,7 +12,7 @@ public class CsvReaderTest {
     
     @Test
     public void EmployeeReaderTest(){
-        EmployeeReader reader = new EmployeeReader();
+        EmployeeReader reader = new EmployeeReader("employee.csv");
 
         List<Employee> employees = reader.readEmployee();
 
@@ -20,5 +21,56 @@ public class CsvReaderTest {
         assertEquals((Integer)28, employees.get(1).getAge());
         assertEquals(Date.valueOf("1978-4-1"), employees.get(0).getBirth());
         assertEquals(null, employees.get(1).getNote());
+    }
+
+    @Test
+    public void WriteEmployeeTest(){
+        EmployeeReader reader = new EmployeeReader("employeeWrite.csv");
+
+        List<Employee> employees = new ArrayList<>();
+        
+        Employee employee1 = new Employee();
+        employee1.setName("hello");
+        employee1.setAge(33);
+        employee1.setBirth(Date.valueOf("2021-11-11"));
+        employee1.setEmail("sugimomoto@example.com");
+        employee1.setNote("note");
+
+        employees.add(employee1);
+
+        Employee employee2 = new Employee();
+        employee2.setName("hello2");
+        employee2.setAge(34);
+        employee2.setBirth(Date.valueOf("2021-11-12"));
+        employee2.setEmail("sugimomoto2@example.com");
+        employee2.setNote("note2");
+
+        employees.add(employee2);
+
+        reader.writeEmployee(employees);
+
+        employees = reader.readEmployee();
+        assertEquals(2, employees.size());
+
+    }
+
+    @Test
+    public void ReferenceTest(){
+        List<Employee> employees = new ArrayList<>();
+        Employee employee = new Employee();
+        employee.setName("hello1");
+
+        employees.add(employee);
+
+        assertEquals("hello1", employees.get(0).getName());
+
+        employee = new Employee();
+        employee.setName("hello2");
+
+        employees.add(employee);
+
+        assertEquals(2, employees.size());
+        assertEquals("hello1", employees.get(0).getName());
+        assertEquals("hello2", employees.get(1).getName());
     }
 }
