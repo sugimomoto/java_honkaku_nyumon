986051d3ab0432fbb570b40e11f8e74e03fe3820
sugimomoto <sugimomoto@gmail.com>
2021-12-02 08:35:40 +0900

    Added chapter 14_3_1

---
+++ src/main/java/chapter_14_3_1/JsonManager.java
@@ -0,0 +1,28 @@
+package chapter_14_3_1;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonManager {
+
+    private String path;
+
+    public JsonManager(String filePath){
+        path = filePath;
+    }
+
+    public Employee readEmployeeFromJson() throws StreamReadException, DatabindException, IOException{
+        File file = new File(path);
+
+        ObjectMapper mapper = new ObjectMapper();
+        Employee employee = mapper.readValue(file, Employee.class);
+
+        return employee;
+    }
+}
